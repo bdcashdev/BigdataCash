3ce952124a8ce6fa16680fa1a3bcc1a8821ec02f
TFinch <travisfinch01@gmail.com>
2021-03-26 22:42:16 -0400

    add  =

--- src/main.cpp
+++ src/main.cpp
@@ -1600,27 +1600,27 @@ int64_t GetBlockValue(int nHeight)
     int64_t nSubsidy = 0;
 
     if (ActiveProtocol() >= REWARD_CHANGE) {
-        if (nHeight > 300000)  nSubsidy = .25 * COIN;
-        if (nHeight > 200000)  nSubsidy = .5  * COIN;
-        if (nHeight > 150000)  nSubsidy = .75 * COIN;
-        if (nHeight > 100000)  nSubsidy = 1   * COIN;
-        if (nHeight > 80000)   nSubsidy = 2   * COIN;
-        if (nHeight > 60000)   nSubsidy = 3   * COIN;
-        if (nHeight > 40000)   nSubsidy = 4   * COIN;
-        if (nHeight > 30000)   nSubsidy = 1   * COIN;
-        if (nHeight > 20000)   nSubsidy = 3   * COIN;
-        if (nHeight > 10000)   nSubsidy = 3   * COIN;
-        if (nHeight > 1)       nSubsidy = 2   * COIN;
+        if (nHeight >= 300000)  nSubsidy = .25 * COIN;
+        if (nHeight >= 200000)  nSubsidy = .5  * COIN;
+        if (nHeight >= 150000)  nSubsidy = .75 * COIN;
+        if (nHeight >= 100000)  nSubsidy = 1   * COIN;
+        if (nHeight >= 80000)   nSubsidy = 2   * COIN;
+        if (nHeight >= 60000)   nSubsidy = 3   * COIN;
+        if (nHeight >= 40000)   nSubsidy = 4   * COIN;
+        if (nHeight >= 30000)   nSubsidy = 1   * COIN;
+        if (nHeight >= 20000)   nSubsidy = 3   * COIN;
+        if (nHeight >= 10000)   nSubsidy = 3   * COIN;
+        if (nHeight >= 1)       nSubsidy = 2   * COIN;
         if (nHeight == 0)      nSubsidy = 1000000 * COIN;
     }else{
-        if (nHeight > 100000)  nSubsidy = 1 * COIN;
-        if (nHeight > 80000)   nSubsidy = 2 * COIN;
-        if (nHeight > 60000)   nSubsidy = 3 * COIN;
-        if (nHeight > 40000)   nSubsidy = 4 * COIN;
-        if (nHeight > 30000)   nSubsidy = 1 * COIN;
-        if (nHeight > 20000)   nSubsidy = 3 * COIN;
-        if (nHeight > 10000)   nSubsidy = 3 * COIN;
-        if (nHeight > 1)       nSubsidy = 2 * COIN;
+        if (nHeight >= 100000)  nSubsidy = 1 * COIN;
+        if (nHeight >= 80000)   nSubsidy = 2 * COIN;
+        if (nHeight >= 60000)   nSubsidy = 3 * COIN;
+        if (nHeight >= 40000)   nSubsidy = 4 * COIN;
+        if (nHeight >= 30000)   nSubsidy = 1 * COIN;
+        if (nHeight >= 20000)   nSubsidy = 3 * COIN;
+        if (nHeight >= 10000)   nSubsidy = 3 * COIN;
+        if (nHeight >= 1)       nSubsidy = 2 * COIN;
         if (nHeight == 0)      nSubsidy = 1000000 * COIN;
     }
     // Check if we reached the coin max supply.
